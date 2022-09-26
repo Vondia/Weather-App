0433d62a2c38872db6ffe0ed798c0c09582c4550
Vondia <pimvdbosch@gmail.com>
2022-09-26 15:38:20 +0200

    style changes

--- components/MainWeather.tsx
+++ components/MainWeather.tsx
@@ -11,6 +11,8 @@ type MainWeatherProps = {
   maxDegrees: number;
   humidity: number;
   feelsLike: number;
+  rain?: number;
+  weatherMain: string;
 };
 
 export const MainWeather: FC<MainWeatherProps> = ({
@@ -22,6 +24,8 @@ export const MainWeather: FC<MainWeatherProps> = ({
   maxDegrees,
   humidity,
   feelsLike,
+  rain,
+  weatherMain,
 }) => {
   return (
     <>
@@ -35,7 +39,11 @@ export const MainWeather: FC<MainWeatherProps> = ({
         <div className={text}>
           <h3>{description}</h3>
           <h3>huidige temp:{Math.round(degrees)}°C</h3>
-          <h3>Luchtvochtigheid: {humidity}%</h3>
+          {weatherMain === "Rain" ? (
+            <h3>{rain}mm/u regen</h3>
+          ) : (
+            <h3>Luchtvochtigheid: {humidity}%</h3>
+          )}
           <h3>min temp: {Math.round(minDegrees)}°C</h3>
           <h3>Gevoelstemp: {Math.round(feelsLike)}°C</h3>
           <h3>max temp: {Math.round(maxDegrees)}°C</h3>
@@ -50,7 +58,7 @@ const parent = css`
   flex-direction: column;
   align-items: center;
   width: fit-content;
-  border-radius: 1rem 1rem 1rem 0;
+  border-radius: 1rem;
   overflow: hidden;
 `;
 
@@ -62,11 +70,12 @@ const image = css`
 const text = css`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  font-size: 1em;
+  font-size: 0.85em;
+  padding: 0.5rem;
   width: 100%;
   color: white;
   background-color: #6a47ff90;
-  margin-top: -3px;
+  margin-top: -4px;
   opacity: 0;
   transform: translateY(5rem);
   transition: all 1s cubic-bezier(0.08, 0.62, 0.25, 1);
