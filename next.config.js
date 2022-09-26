07e00df1c301203a3fbc2f3e77be480f2ce59297
Vondia <pimvdbosch@gmail.com>
2022-09-26 15:38:43 +0200

    change to get the openweather icons

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // next.config.js
 const withLinaria = require('next-linaria');
 module.exports = withLinaria({
-  /* config options here */
+    images: {
+        domains: [
+          'openweathermap.org'
+        ]
+      }
 });
