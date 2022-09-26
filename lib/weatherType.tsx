70bc04a753f68aeae96b379fb1fd0ba7bef7eecc
Vondia <pimvdbosch@gmail.com>
2022-09-26 15:38:03 +0200

    remade app to forecast to get data for next days

---
+++ lib/weatherType.tsx
@@ -0,0 +1,29 @@
+export interface weatherForecast {
+  list: [
+    {
+      main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        humidity: number;
+      };
+      dt_txt: string;
+      weather: [
+        {
+          id: number;
+          main: string;
+          description: string;
+          icon: string;
+        }
+      ];
+      rain: {
+        "3h": number;
+      };
+    }
+  ];
+  city: {
+    name: string;
+  };
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,18 @@ import { useState } from "react";
 import { getWeatherPicture } from "../lib/getWeatherPicture";
 import { MainWeather } from "../components/MainWeather";
 import { Layout } from "../components/Layout";
-
-interface weatherData {
-  main: {
-    temp: number;
-    feels_like: number;
-    temp_min: number;
-    temp_max: number;
-    pressure: number;
-    humidity: number;
-  };
-  weather: [
-    {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }
-  ];
-  name: string;
-}
+import { GoSearch } from "react-icons/go";
+import { css } from "@linaria/core";
+import { weatherForecast } from "../lib/weatherType";
+import { ForecastWidget } from "../components/ForecastWidget";
+import { getNextDay } from "../lib/getNextDay";
 
 const Home: NextPage = () => {
   const [city, setCity] = useState("");
-  const [weather, setWeather] = useState({} as weatherData);
+  const [weather, setWeather] = useState({} as weatherForecast);
   const [loading, setLoading] = useState(false);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=nl&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=nl&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`;
 
   const fetchWeather = (e: React.SyntheticEvent<EventTarget>) => {
     e.preventDefault();
@@ -50,7 +35,11 @@ const Home: NextPage = () => {
     setCity("");
   };
 
-  const src = getWeatherPicture(weather.weather?.[0].main);
+  const src = getWeatherPicture(weather.list?.[0].weather?.[0].main);
+
+  const forecastList = weather.list;
+  const nextDayList = getNextDay();
+  const forecastFilteredDays = nextDayList?.map((day) => forecastList?.[day]);
 
   return (
     <div>
@@ -59,41 +48,95 @@ const Home: NextPage = () => {
           <title>Weer app</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <h1 style={{ height: "10vh", marginTop: "1rem" }}>Weer app</h1>
-        <form onSubmit={fetchWeather}>
+        <h1 className={title}>Wéér</h1>
+        <form onSubmit={fetchWeather} className={form}>
           <input
             type="text"
             placeholder="Zoek stad"
             value={city}
             onChange={(e) => setCity(e.target.value)}
-            style={{
-              backgroundColor: "transparent",
-              borderRadius: "6px",
-              border: "1px solid #1f293750",
-              marginRight: "0.5rem",
-            }}
+            className={input}
           />
-          <button type="submit">Zoeken</button>
+          <button type="submit" className={button}>
+            <GoSearch />
+          </button>
         </form>
         {loading ? (
           <h2>Loading...</h2>
         ) : (
-          weather.main && (
+          weather.list?.[0].main && (
             <MainWeather
               src={src}
-              city={weather.name}
-              description={weather.weather[0].description}
-              degrees={weather.main.temp}
-              minDegrees={weather.main.temp_min}
-              maxDegrees={weather.main.temp_max}
-              humidity={weather.main.humidity}
-              feelsLike={weather.main.feels_like}
+              city={weather.city.name}
+              description={weather.list?.[0].weather[0].description}
+              degrees={weather.list?.[0].main.temp}
+              minDegrees={weather.list?.[0].main.temp_min}
+              maxDegrees={weather.list?.[0].main.temp_max}
+              humidity={weather.list?.[0].main.humidity}
+              feelsLike={weather.list?.[0].main.feels_like}
+              rain={weather.list?.[0].rain?.["3h"]}
+              weatherMain={weather.list?.[0].weather[0].main}
             />
           )
         )}
+        <div className={forecastDisplay}>
+          {weather.list?.[0].main &&
+            forecastFilteredDays?.map((weather, index) => (
+              <ForecastWidget
+                key={index}
+                date={weather?.dt_txt.slice(0, 11)}
+                src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}
+                description={weather?.weather[0].description}
+                minDegrees={weather?.main.temp_min}
+                maxDegrees={weather?.main.temp_max}
+                feelsLike={weather?.main.feels_like}
+              />
+            ))}
+        </div>
       </Layout>
     </div>
   );
 };
+
+const title = css`
+  height: 10vh;
+  margin-top: 1rem;
+  font-weight: bold;
+  font-size: 3em;
+`;
+
+const form = css`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: fit-content;
+`;
+
+const input = css`
+  background-color: transparent;
+  border-radius: 6px;
+  border: 1px solid #1f293750;
+  padding: 0.5rem 1.25rem 0.5rem 0.5rem;
+`;
+
+const button = css`
+  position: absolute;
+  right: 0;
+  margin-right: 0.25rem;
+  margin-top: 0.5rem;
+  z-index: 10;
+`;
+
+const forecastDisplay = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  margin-top: 3rem;
+  width: fit-content;
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
 
 export default Home;
